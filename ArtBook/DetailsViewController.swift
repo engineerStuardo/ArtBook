98245319bb128d53da5e7144d4b431ae3137b85c
engineerStuardo <engeniero.stuardo@gmail.com>
2023-03-26 14:06:12 -0600

    making the functionality for picking up image from gallery and adding the correct permission on info.plist for older iOS versions

--- ArtBook/DetailsViewController.swift
+++ ArtBook/DetailsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var yearText: UITextField!
     @IBOutlet weak var artistText: UITextField!
@@ -20,6 +20,23 @@ class DetailsViewController: UIViewController {
         // Do any additional setup after loading the view.
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         self.view.addGestureRecognizer(gestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageGR = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageGR)
+    }
+    
+    @objc func selectImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
     }
 
     @objc func hideKeyboard() {
